19c817b9b3b40323c7e4a0453e8942326412f526
Radheshyam gupta <radheshyamjio52@gmail.com>
2024-03-27 20:38:44 +0530

    write e2e test for search functionality

---
+++ e2e-tests/tests/searchHotels.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+const UI_URL = "http://localhost:5173/"
+
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(UI_URL);
+
+    //get the sign in button
+    await page.getByRole('link', { name: "Sign In" }).click()
+
+    await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible()
+    await page.locator("[type=email]").fill("1@1.com")
+    await page.locator("[type=password]").fill("123456")
+
+    await page.getByRole("button", { name: "Sign in" }).click()
+    await expect(page.getByText("Login Success")).toBeVisible()
+    await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible()
+    await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible()
+    await expect(page.getByRole("button", { name: "Log out" })).toBeVisible()
+})
+
+test("should allow user to search  hotel", async ({ page }) => {
+    await page.goto(UI_URL);
+    await page.getByPlaceholder("where are you going").fill("Dublin")
+    await page.getByRole("button", { name: "Search" }).click()
+
+    await expect(page.getByText("Hotels found in Dublin")).toBeVisible()
+    await expect(page.getByText("Dublin Getaways")).toBeVisible()
+})
